d802ed6ad7bc5dd5c13cf51af93dfedbb4e3cbe9
Artur Żak <artur.zak.pl@gmail.com>
2023-08-08 00:15:51 +0200

    Added code for changing images for DarkMode

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-let darkTheme = document.getElementById("themeSwitch");
-let body = document.getElementById("body");
-let line = document.querySelectorAll(".line");
-let visitedLinkElements = document.querySelectorAll(".link:visited");
-let unvisitedLinkElements = document.querySelectorAll(".link:link");
+const darkTheme = document.getElementById("themeSwitch");
+const body = document.body;
+const line = document.querySelectorAll(".line");
+const visitedLinkElements = document.querySelectorAll(".link:visited");
+const unvisitedLinkElements = document.querySelectorAll(".link:link");
+const moonButton = darkTheme.querySelector("img");
 
 darkTheme.addEventListener("click", () => {
   darkTheme.classList.toggle("light");
@@ -13,7 +14,7 @@ darkTheme.addEventListener("click", () => {
     darkTheme.style.opacity = "1";
 
     line.forEach((line) => {
-      line.style.background = "white";  
+      line.style.background = "white";
     });
 
     visitedLinkElements.forEach((visitedLinkElements) => {
@@ -23,10 +24,12 @@ darkTheme.addEventListener("click", () => {
     unvisitedLinkElements.forEach((unvisitedLinkElements) => {
       unvisitedLinkElements.style.color = "#4091BA";
     });
+
+    // Update the button's image source for dark mode
+    moonButton.src = "https://i.postimg.cc/5yqJc0JW/Light-Moon.png";
   } else {
     body.style.background = "white";
     body.style.color = "black";
-    darkTheme.style.opacity = "1";
 
     line.forEach((line) => {
       line.style.background = "black";
@@ -39,5 +42,8 @@ darkTheme.addEventListener("click", () => {
     unvisitedLinkElements.forEach((unvisitedLinkElements) => {
       unvisitedLinkElements.style.color = "#26586f";
     });
+
+    // Update the button's image source for light mode
+    moonButton.src = "https://i.postimg.cc/tTcZ1Bn4/Dark-Moon.png";
   }
 });
